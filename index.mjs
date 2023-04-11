2e059e6f8c0fb336507ecc4a55f6b74e5f7536a9
erixweb <erikmorenodev@gmail.com>
2023-04-11 13:55:50 +0200

    first commit

---
+++ index.mjs
@@ -0,0 +1,38 @@
+import { stringsAreSimilar } from "./similarities.mjs"
+
+// Mnesajes admitidos
+const messages = [
+    {
+        input: "¿Hola, qué tal?",
+        answer: "¡Estoy bien! Muchas gracias"
+    },
+    {
+        input: "¿Cómo te llamas?",
+        answer: "Me llamo Erik 😁"
+    },
+    {
+        input: "¿Usas React?",
+        answer: "Sí, uso React en muchos de mis proyectos. Aunque, muchos también están hechos con Astro & HTML sin ningún framework/librería."
+    },
+    {
+        input: "¿Qué tecnologías utilizas?",
+        answer: "Uso React en muchos de mis proyectos. Aunque, muchos también están hechos con Astro & HTML sin ningún framework/librería. También utilizo PHP, es la tecnología en la que más estoy especializado."
+    },
+    {
+        input: "¿Qué utilizas?",
+        answer: "Uso React en muchos de mis proyectos. Aunque, muchos también están hechos con Astro & HTML sin ningún framework/librería. También utilizo PHP, es la tecnología en la que más estoy especializado."
+    },
+    {
+        input: "¿Con qué está hecha tu página?",
+        answer: "Mi página web está hecha con Astro. Antes estaba hecha con HTML sin ninguna librería pero recientemente la he pasado a AstroJS. Tengo un blog sobre ello https://erik.pages.dev/articulo/cambio-a-astro"
+    }
+]
+
+const input = "Con que está hecha la pagina"
+
+
+for (let index = 0; index < messages.length; index++) {
+    const item = messages[index];
+    
+    if (stringsAreSimilar(input, item.input)) console.log(item.answer)
+}
